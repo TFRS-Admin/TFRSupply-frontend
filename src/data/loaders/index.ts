1f914224fca105752e2009871d537f08d938b727
TFRS-Admin <admin@tx1rs.com>
2026-06-30 18:23:56 -0500

    Add typed product data platform loaders

---
+++ src/data/loaders/index.ts
@@ -0,0 +1,4 @@
+export { loadTypedCategory, listTypedCategories } from './categoryLoader';
+export { loadTypedConfigurator, listTypedConfigurators } from './configuratorLoader';
+export { loadTypedProduct, listTypedProducts } from './productLoader';
+export { loadTypedVertical, listTypedVerticals } from './verticalLoader';
